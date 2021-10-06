53391d48f11628c397065786c60065960c40c6c3
Zayyan Faizal <zfaizal2@illinois.edu>
2021-10-06 12:14:22 -0500

    init hardhat

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# nft-mint
+#nft-mint
+
+
+# Setup
+To install dependencies...
+```
+npm install
+```
+
+To run test a deploy
+```
+npx hardhat run scripts/run.js
+```
--- contracts/myNFT.sol
+++ contracts/myNFT.sol
@@ -1,7 +1,7 @@
 //Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -11,7 +11,7 @@ contract MyNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("MyNFT", "NFT") {}
+    constructor() ERC721("MyNFT", "NFT") {}
 
     function mintNFT(address recipient, string memory tokenURI)
         public onlyOwner
